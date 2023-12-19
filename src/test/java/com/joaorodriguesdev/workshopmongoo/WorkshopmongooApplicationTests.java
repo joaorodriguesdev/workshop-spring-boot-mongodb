b3b1eb0721673f85854c72c64fbba10b1744c05e
Joao Rodrigues <joaolopesgoularte@gmail.com>
2023-12-19 17:30:27 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/joaorodriguesdev/workshopmongoo/WorkshopmongooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaorodriguesdev.workshopmongoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
