dc28d0c72436e212bc8bde2e61f408c44aa0723f
Joao Rodrigues <joaolopesgoularte@gmail.com>
2024-03-29 10:50:41 -0300

    Obtendo um usuário por id

--- src/main/java/com/joaorodriguesdev/workshopmongoo/services/UserService.java
+++ src/main/java/com/joaorodriguesdev/workshopmongoo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.joaorodriguesdev.workshopmongoo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaorodriguesdev.workshopmongoo.domain.User;
 import com.joaorodriguesdev.workshopmongoo.repository.UserRepository;
+import com.joaorodriguesdev.workshopmongoo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,8 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
