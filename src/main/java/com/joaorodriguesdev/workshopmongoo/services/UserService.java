f2baeef07a909b2076c916235a713f53f50c5322
Joao Rodrigues <joaolopesgoularte@gmail.com>
2024-03-30 10:48:17 -0300

    Inserção de usuário com POST

--- src/main/java/com/joaorodriguesdev/workshopmongoo/services/UserService.java
+++ src/main/java/com/joaorodriguesdev/workshopmongoo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaorodriguesdev.workshopmongoo.domain.User;
+import com.joaorodriguesdev.workshopmongoo.dto.UserDTO;
 import com.joaorodriguesdev.workshopmongoo.repository.UserRepository;
 import com.joaorodriguesdev.workshopmongoo.services.exception.ObjectNotFoundException;
 
@@ -23,4 +24,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
